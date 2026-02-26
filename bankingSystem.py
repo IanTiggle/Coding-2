1b6f461874dd64a0147496a4fa0c642c8ffe3dcd
Ian Tiggle <itiggle301@boyslatin.net>
2026-02-26 15:26:16 +0000

    Class Activity

--- bankingSystem.py
+++ bankingSystem.py
@@ -1,3 +1,6 @@
+import random
+
+
 class IanFirstBank:
     def __init__(self, name, age, balance, accountNumber, phone):
         self.name = name
@@ -22,7 +25,38 @@ account_1 = IanFirstBank("Bob", 45, 300, "124ab34", "NA")
 account_2 = IanFirstBank("Sally", 25, 100, "154av34", "NA")
 
 account_1.checkBalance()
-# account_2.checkBalance()
-
 account_1.deposit(500)
 account_1.withdraw(150)
+
+account_2.checkBalance()
+account_2.deposit(200)
+account_2.withdraw(50)
+
+bankUsers = []
+
+def CreateAccount():
+    print("Welcome to Ian's First Bank")
+    print("Please fill out the following information to create your account")
+    name = input("Please type in your name: ")
+    age = int(input("Please type in your age: "))
+    balance = int(input("Please type in how much you want to deposit: "))
+    accountNumber = random.randrange(100,500)
+
+    print("Congrats! Your account has been created")
+
+    account_1 = IanFirstBank(name, age, balance, accountNumber, "NA")
+    bankUsers.append(account_1)
+    return account_1
+print(bankUsers)
+
+
+
+def mainBankApp():
+    print("Welcome")
+    print("Are you a new user?")
+    print("Please type 1 for existing user or 2 for new user")
+    answer = int(input())
+    if answer == 1:
+        print("Welcome back")
+    elif answer == 2:
+        CreateAccount()

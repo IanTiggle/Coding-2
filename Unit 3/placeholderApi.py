faedafb098430303382229d0a79664c8664ddd42
Ian Tiggle <itiggle301@boyslatin.net>
2026-04-14 14:24:59 +0000

    Class activity

--- Unit 3/placeholderApi.py
+++ Unit 3/placeholderApi.py
@@ -17,12 +17,30 @@ import requests
 
 
 #print(response.text)
+def pokedex():
+    search = input("would you like to search pokemon, ")
+    while search == "y":
+        pokename = input("please enter a pokemon name, ")
+        url = f'https://pokeapi.co/api/v2/pokemon/{pokename}'
+        response = requests.get(url)
 
 
-url = 'https://pokeapi.co/api/v2/berry-firmness/2/'
-myobj = { "title": "foo", "body": "bar", "userId": 1 }
-
-response = requests.get(url)
 
 
-print(response.text)
+        if response.status_code == 200:
+            data= response.json()
+
+            filtered_data = {
+                "name": data["name"],
+                "height": data["height"],
+                "weight": data["weight"], 
+                "types": data["types"],
+                "abilities": [ability["ability"]["name"] for ability in data["abilities"]]
+            }
+            print(filtered_data)
+            search = input("would you like to find another pokemon?  ")
+
+        else:
+            print("data not found")
+            print(response.status_code)
+pokedex()

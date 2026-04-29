979c2914bbfa9b5da4312effa1d66879bf491cc1
Ian Tiggle <itiggle301@boyslatin.net>
2026-04-29 14:26:42 +0000

    Class Activity

--- Test.py
+++ Test.py
@@ -7,3 +7,19 @@ def planeTicket(hasRoom, hasPlaneTicket):
         return " You are not eligible for a plane ticket"
     
 print(planeTicket(False, True))
+
+#Create a movie class. Your movie class should include 4 parameters.
+#Then create 3 move objects from your class.
+
+class movie:
+    def __init__(self, title, director, genre, release_year):
+        self.title = title
+        self.director = director
+        self.genre = genre
+        self.release_year = release_year
+
+movie1 = movie("Daredevil", "Mark Steven Johnson", "Action", 2003)
+movie2 = movie("The Dark Knight", "Christopher Nolan", "Action", 2008)
+movie3 = movie("Home", "Tim Johnson", "Animation", 2015)
+
+#https://web.dragonball-api.com/
---
+++ Unit 3/sql_Intro_.py
@@ -0,0 +1,30 @@
+# 1. bring in the sql lite module to get access to all the sql methods (functions) to work with our database.
+import sqlite3
+
+# 2. We need to establish an actual database file using the connect method.
+connect = sqlite3.connect('myDb.db')
+
+# 3. Create a object that will be sent to the new database.
+cursor = connect.cursor()
+
+# 4. We need to create a schema (structure) for our data.
+# cursor.execute('''
+#                CREATE TABLE gameSales(
+#                id INTEGER PRIMARY KEY,
+#                name TEXT,
+#                platform TEXT, 
+#                developer TEXT,
+#                price INTEGER,
+#                genre TEXT,
+#                totalSales INTEGER
+#                )
+#                ''')
+# 5. we can now create our first database object.
+cursor.execute('''
+               INSERT INTO gameSales(name, platform, developer, price, genre, totalSales)
+                VALUES('Marvel Rivals', 'PC', 'Marvel', 0, 'Hero Shooter', 1360000)
+                ''')
+
+# 6. once we have a new data object, we need to commit it to the database.
+connect.commit()
+connect.close()

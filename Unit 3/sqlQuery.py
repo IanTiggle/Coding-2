da0ad85219ec0fbdbb00a0d6c0b16b822db4cb81
Ian Tiggle <itiggle301@boyslatin.net>
2026-05-01 14:19:53 +0000

    Class Activity

---
+++ Unit 3/sqlQuery.py
@@ -0,0 +1,38 @@
+# imports all the methods we will need to send/ receive data from the database
+import sqlite3
+
+# just a function
+def getAllData():
+    # setting up the connection to our database (the road)
+    # to/from the database (the vehicle)
+    connect = sqlite3.connect('myDb.db')
+    cursor = connect.cursor()
+
+    # the data we want to get
+    # SELECT = our selection
+    query = 'SELECT  platform FROM gameSales'
+
+    # turns the vehicle on to get the data
+    cursor.execute(query)
+
+    # tells the cursor to fetch/ get ALL data
+    results = cursor.fetchall()
+
+    # shows our results in terminal
+    print(results)
+
+#getAllData()
+
+def get1data():
+    connect = sqlite3.connect('myDb.db')
+    
+    cursor = connect.cursor()
+
+    query = "SELECT name FROM gameSales WHERE developer = 'Bethesda'"
+
+    cursor.execute(query)
+
+    result = cursor.fetchone()
+
+    print(result)
+get1data()
